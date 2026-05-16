6f41982a4346a6f3956643ec829480b3d392658e
SturkaCircutz <313721325sjw@gmail.com>
2026-05-16 15:10:10 -0600

    done with device buffer

--- include/device_buffer.hpp
+++ include/device_buffer.hpp
@@ -45,10 +45,55 @@ namespace vllm{
 
             DeviceBuffer& operator=(DeviceBuffer && other) noexcept
             {
+
+//DeviceBuffer<float> a(1024);
+//DeviceBuffer<float> b(2048);
+// b = std::move(a);
+// a.ptr GPU mem 0xaaa
+// a.size = 1024
+// b.ptr gpu mem 0xbbb
+// b,size 2048
+
+//exchange b.ptr = a.ptr
+// b size = 1024, a size = 0
                 if(this != & other){
                     reset();
-                    p
+                    ptr_ = std::exchange(other.ptr_, nullptr);
+                    size_ = std::exchange(other.size, 0);
+                }
+                return *this;
+            }
+
+
+            // allocate the mem
+            void allocate(size_t count){
+                reset();
+
+                if(count==0) return 0;
+
+                CUDA_CHECK(cudaMalloc(&ptr_, count * sizeof(T)));
+                size_ = count;
+            }
+
+            // reset the mem
+            void reset(){
+                if(ptr_ != nullptr){
+                    CUDA_CHECK(cudaFree(ptr_));
+                    ptr_ = nullptr;
+                    size_ = 0;
                 }
             }
-    }
+
+            T*data() return ptr_; // return gpu buffer row pointer
+            const T*data() const return ptr_;
+            size_t size() const {
+                return size_;
+            }
+
+            private:
+            T*ptr_ = nullptr;
+            size_t size_ = 0;
+
+
+    };
 }
